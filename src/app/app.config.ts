a4fa00877780969609432b3b7ee4837382c5f842
wiiiky <wiiiky@outlook.com>
2018-05-17 22:56:40 +0800

    add prod host

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import { isDevMode } from '@angular/core';
 export class APIConfig {
   static HOST:string;
   static DEV_HOST = "http://127.0.0.1:1323/api";
-  static PROD_HOST = "http://127.0.0.1:1323/api";
+  static PROD_HOST = "https://10.wikylyu.me/api";
   static PATH_SUPPORTED_COUNTRIES = "/supported_countries";
   static PATH_ACCOUNT_INFO = "/account";
   static PATH_SIGNUP = "/account";
   static PATH_LOGIN = "/account/token";
   static PATH_LOGOUT = "/account/token";
   static PATH_QUESTIONES = "/questions";
+  static PATH_FILE = "/file";
 }
 
 if(isDevMode()){
