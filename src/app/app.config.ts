2d82aa863efef967dea91ab45e7862242f8901f3
wiiiky <wiiiky@outlook.com>
2018-05-19 11:26:57 +0800

    fix config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { environment } from '../environments/environment';
 export class APIConfig {
   static HOST:string;
   static DEV_HOST = "http://127.0.0.1:1323/api";
-  static PROD_HOST = "https://10.wikylyu.me/api";
+  static PROD_HOST = "https://10.wikylyu.xyz/api";
   static PATH_SUPPORTED_COUNTRIES = "/supported_countries";
   static PATH_ACCOUNT_INFO = "/account";
   static PATH_SIGNUP = "/account";
